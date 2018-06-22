8c3134b7ad6ee4e2d5d8194d3789cb186fb732fc
unknown <smrserg2017@gmail.com>
2018-06-22 14:23:59 +0300

    final commit

---
+++ build/js/scripts.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var btn_prev = document.querySelector('.arr-prev');
+var btn_next = document.querySelector('.arr-next');
+var reviews = document.querySelectorAll('.review-block__card');
+var counters = document.querySelectorAll('.review-block__page-counter span');
+
+var review = 0;
+var counter = 0;
+
+var prevClickHandler = function prevClickHandler() {
+	goToReview(review - 1);
+	goToCount(counter - 1);
+};
+
+var nextClickHandler = function nextClickHandler() {
+	goToReview(review + 1);
+	goToCount(counter + 1);
+};
+
+var goToCount = function goToCount(n) {
+	counters[counter].classList.remove('through');
+	counter = (n + counters.length) % counters.length;
+	counters[counter].classList.add('through');
+};
+
+var goToReview = function goToReview(n) {
+	reviews[review].style.display = 'none';
+	review = (n + reviews.length) % reviews.length;
+	reviews[review].style.display = 'flex';
+};
+
+btn_prev.addEventListener('click', prevClickHandler);
+btn_next.addEventListener('click', nextClickHandler);
---
+++ src/js/scripts.js
@@ -0,0 +1,32 @@
+let btn_prev = document.querySelector('.arr-prev');
+let btn_next = document.querySelector('.arr-next');
+let reviews = document.querySelectorAll('.review-block__card');
+let counters = document.querySelectorAll('.review-block__page-counter span');
+
+let review = 0;
+let counter = 0;
+
+const prevClickHandler = () => {
+  goToReview(review - 1);
+  goToCount(counter - 1);
+}
+
+const nextClickHandler = () => {
+  goToReview(review + 1);
+  goToCount(counter + 1);
+}
+
+const goToCount = (n) => {
+	counters[counter].classList.remove('through');
+	counter = (n + counters.length) % counters.length;
+	counters[counter].classList.add('through');
+};
+
+const goToReview = (n) => {
+	reviews[review].style.display = 'none';
+	review = (n + reviews.length) % reviews.length;
+	reviews[review].style.display = 'flex';
+};
+
+btn_prev.addEventListener('click', prevClickHandler);
+btn_next.addEventListener('click', nextClickHandler);
